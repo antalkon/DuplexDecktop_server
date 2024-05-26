4a341ef355ef58107b03276cea332f0e67d32c16
Antipev Aleksei <87186332+antalkon@users.noreply.github.com>
2024-05-26 16:56:33 +0500

    мммм

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error init %s", err.Error())
+	}
+
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
